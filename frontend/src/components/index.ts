f0bcdad921053d2a7f9d3e16a0324e0c6fbb65f6
Alvaro A. Alvarez <alvaro1@stanford.edu>
2024-10-15 22:21:22 +0000

    remove unused files

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,10 +2,6 @@
 
 export { default as LanguageSelector } from "./LanguageSelector";
 export { default as SearchBox } from "./SearchBox";
-export { default as Disambiguation } from "./Disambiguation";
-export { default as SynonymList } from "./SynonymList";
-export { default as MedicalConceptsTable } from "./MedicalConceptsTable";
-export { default as Recommendation } from "./Recommendation";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as LoadingComponent } from "./LoadingComponent";
